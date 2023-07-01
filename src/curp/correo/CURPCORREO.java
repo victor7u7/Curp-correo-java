43fc3961f4dc10ba191988fdccc9fc30cacbc43a
victor7u7 <victorisraelmg@gmail.com>
2023-06-30 22:50:54 -0600

    primer commit

---
+++ src/curp/correo/CURPCORREO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package curp.correo;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import javax.swing.text.Document;
+
+/**
+ *
+ * @author israe
+ */
+public class CURPCORREO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ventanaCurp().setVisible(true);
+    }
+    
+}
